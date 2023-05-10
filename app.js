699854da119eed3bfd3f0420220112c63c711a64
Meagan Provencher <mrprov@mac.com>
2023-05-10 12:18:39 -0600

    Split out /ping route && add Swagger init

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const axios = require('axios');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
-const packageJson = require('./package.json');
+const ping = require('./routes/ping');
 
 const PORT = 30000;
 
@@ -10,26 +12,46 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/ping', ping);
+
 // Load page
 app.get("/", function (req, res) {
     res.status(200).send()
 });
 
-app.post("/ping", function (req, res) {
-    const { message } = req.body;
 
-    axios.get(`https://postman-echo.com/get?message=${message}`)
-    .then((response) => {
-        console.log(process.env.NODE_ENV)
-        res.status(200).send({
-            echo: response.data,
-            timestamp: Date.now(),
-            env: process.env.NODE_ENV,
-            version: packageJson.version,
-        });
-    });
-
-});
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Meagan Provencher - Vail Take Home",
+        version: "0.1.0",
+        description:
+          "This is a simple API application made with Express and documented with Swagger",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "Meagan Provencher",
+          email: "meaganprovencher@gmail.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:30000",
+        },
+      ],
+    },
+    apis: ["./routes/*.js"],
+  };
+  
+  const specs = swaggerJsdoc(options);
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
 
 app.listen(PORT, function () {
     console.log(`Server is running on localhost:${PORT}`);
